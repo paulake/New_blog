21442b1e50b35845b0e8fbe59efa67bc72b9bf65
paulasalurand <paula.salurand@gmail.com>
2018-02-14 17:13:13 +0000

    300.mp4

--- blog/views.py
+++ blog/views.py
@@ -16,9 +16,9 @@ def post_new(request):
 			post.published_date = timezone.now()
 			post.save()
 			return redirect('post_detail', pk=post.pk)
-		else:
-			form = PostForm()
-		return render(request, 'blog/post_edit.html', {'form': form})
+	else:
+		form = PostForm()
+	return render(request, 'blog/post_edit.html', {'form': form})
     
 def post_list(request):
 	posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
