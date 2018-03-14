ed111668bd7334b5f7bd9583a2dc336292d7a633
paulasalurand <paula.salurand@gmail.com>
2018-03-14 15:53:44 +0000

    added to installed apps

--- Cinema4D/views.py
+++ Cinema4D/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, render_to_response
 
 def index(request):
-	return render_to_response (request, 'Cinema4D/Cinema4D.html')
+	return render (request, 'cinema4d/cinema4d.html')
